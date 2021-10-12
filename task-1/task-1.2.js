cad345fe84a2bc2d3299578ddefb0d0bb49c8160
MCooper253 <morgan_cooper@epam.com>
2021-10-12 17:03:27 +0100

    stream.pipeline implemented

--- task-1/task-1.2.js
+++ task-1/task-1.2.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const csv = require('csvtojson');
+const { pipeline } = require('stream');
 
 const file = 'sample-files/sample.csv';
 
@@ -7,9 +8,21 @@ const readable = fs.createReadStream(file);
 const writeable = fs.createWriteStream('out.txt')
 const transform = csv();
 
-readable.pipe(transform)
-  .on('data', chunk => console.log(`received ${chunk.length} bytes of data.`)) //This is the show how big the chunks that are getting steamed are.
-  .on('error', err => console.log(err))
-  .pipe(writeable)
-  .on('error', err => console.log(err));
+// readable.pipe(transform)
+//   .on('data', chunk => console.log(`received ${chunk.length} bytes of data.`)) //This is the show how big the chunks that are getting steamed are.
+//   .on('error', err => console.log(err))
+//   .pipe(writeable)
+//   .on('error', err => console.log(err));
 
+pipeline(
+  readable,
+  transform,
+  writeable,
+  (err) => {
+    if (err) {
+      console.error('Pipeline failed.', err);
+    } else {
+      console.log('Pipeline succeeded.');
+    }
+  }
+);
